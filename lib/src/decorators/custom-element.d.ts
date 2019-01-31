d697872e6d87b22f4fbddd4d14ec53063b7b550d
Alexander Wende <alex.wende@gmail.com>
2019-01-31 21:28:22 +0100

    chore: update build files

--- lib/src/decorators/custom-element.d.ts
+++ lib/src/decorators/custom-element.d.ts
@@ -1,9 +1,9 @@
 import { CustomElementType } from '../custom-element';
 export interface CustomElementDeclaration extends Object {
-    selector: string;
+    selector?: string;
     shadow?: boolean;
     define?: boolean;
 }
 export declare const DEFAULT_CUSTOM_ELEMENT_DECLARATION: CustomElementDeclaration;
-export declare const customElement: (options: CustomElementDeclaration) => (target: CustomElementType<import("../custom-element").CustomElement>) => void;
+export declare const customElement: (options?: CustomElementDeclaration) => (target: CustomElementType<import("../custom-element").CustomElement>) => void;
 //# sourceMappingURL=custom-element.d.ts.map
