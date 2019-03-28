257e01a25fc229690b8902665934c29b8f885a37
Alexander Wende <alex.wende@gmail.com>
2019-03-28 21:49:15 +0100

    fix: update accordion usage demo

--- demo/src/app.template.ts
+++ demo/src/app.template.ts
@@ -167,7 +167,7 @@ export const template = (element: App) => html`
                 <ui-tab id="tab-1" aria-controls="tab-panel-1"><span>First Tab</span></ui-tab>
                 <ui-tab id="tab-2" aria-controls="tab-panel-2">Second Tab</ui-tab>
                 <ui-tab id="tab-3" aria-controls="tab-panel-3" aria-disabled="true">Third Tab</ui-tab>
-                <ui-tab id="tab-4" aria-controls="tab-panel-4" >Fourth Tab</ui-tab>
+                <ui-tab id="tab-4" aria-controls="tab-panel-4">Fourth Tab</ui-tab>
             </ui-tab-list>
             <ui-tab-panel id="tab-panel-1">
                 <h3>First Tab Panel</h3>
@@ -199,32 +199,34 @@ export const template = (element: App) => html`
 
             <ui-accordion>
 
-                <ui-accordion-panel expanded>
-                    <h3 slot="ui-accordion-panel-header">Panel One</h3>
-                    <div slot="ui-accordion-panel-body">
-                        <p>Lorem ipsum dolor sit amet, no prima qualisque euripidis est. Qualisque quaerendum at est.
-                            Laudem constituam ea usu, virtute ponderum posidonium no eos. Dolores consetetur ex has. Nostro
-                            recusabo an est, wisi summo necessitatibus cum ne.</p>
-                        <p>At usu epicurei assentior, putent dissentiet repudiandae ea quo. Pro ne debitis placerat
-                            signiferumque, in sonet volumus interpretaris cum. Dolorum appetere ne quo. Dicta qualisque eos
-                            ea, eam at nulla tamquam.
-                        </p>
-                    </div>
+                <ui-accordion-panel id="custom-panel-id" expanded level="3">
+
+                    <ui-accordion-header>Panel One</ui-accordion-header>
+
+                    <p>Lorem ipsum dolor sit amet, no prima qualisque euripidis est. Qualisque quaerendum at est.
+                        Laudem constituam ea usu, virtute ponderum posidonium no eos. Dolores consetetur ex has. Nostro
+                        recusabo an est, wisi summo necessitatibus cum ne.</p>
+                    <p>At usu epicurei assentior, putent dissentiet repudiandae ea quo. Pro ne debitis placerat
+                        signiferumque, in sonet volumus interpretaris cum. Dolorum appetere ne quo. Dicta qualisque eos
+                        ea, eam at nulla tamquam.
+                    </p>
+
                 </ui-accordion-panel>
 
-                <ui-accordion-panel>
-                    <h3 slot="ui-accordion-panel-header">Panel Two</h3>
-                    <div slot="ui-accordion-panel-body">
-                        <p>In clita tollit minimum quo, an accusata volutpat euripidis vim. Ferri quidam deleniti quo ea,
-                            duo animal accusamus eu, cibo erroribus et mea. Ex eam wisi admodum praesent, has cu oblique
-                            ceteros eleifend. Ex mel platonem assentior persequeris, vix cibo libris ut. Ad timeam accumsan
-                            est, et autem omnes civibus mel. Mel eu ubique equidem molestiae, choro docendi moderatius ei
-                            nam.</p>
-                        <p>Qui suas solet ceteros cu, pertinax vulputate deterruisset eos ne. Ne ius vide nullam, alienum
-                            ancillae reformidans cum ad. Ea meliore sapientem interpretaris eam. Commune delicata
-                            repudiandae in eos, placerat incorrupte definitiones nec ex. Cu elitr tantas instructior sit,
-                            eu eum alia graece neglegentur.</p>
-                    </div>
+                <ui-accordion-panel level="3">
+
+                    <ui-accordion-header>Panel Two</ui-accordion-header>
+
+                    <p>In clita tollit minimum quo, an accusata volutpat euripidis vim. Ferri quidam deleniti quo ea,
+                        duo animal accusamus eu, cibo erroribus et mea. Ex eam wisi admodum praesent, has cu oblique
+                        ceteros eleifend. Ex mel platonem assentior persequeris, vix cibo libris ut. Ad timeam accumsan
+                        est, et autem omnes civibus mel. Mel eu ubique equidem molestiae, choro docendi moderatius ei
+                        nam.</p>
+                    <p>Qui suas solet ceteros cu, pertinax vulputate deterruisset eos ne. Ne ius vide nullam, alienum
+                        ancillae reformidans cum ad. Ea meliore sapientem interpretaris eam. Commune delicata
+                        repudiandae in eos, placerat incorrupte definitiones nec ex. Cu elitr tantas instructior sit,
+                        eu eum alia graece neglegentur.</p>
+
                 </ui-accordion-panel>
 
             </ui-accordion>
