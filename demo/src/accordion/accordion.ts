e19ac67bbc7ce64db33a928603d029653fc7d2c1
Alexander Wende <alex.wende@gmail.com>
2019-03-29 03:40:17 +0100

    feat: add keyboard navigation to accordion via focus-manager

--- demo/src/accordion/accordion.ts
+++ demo/src/accordion/accordion.ts
@@ -1,25 +1,30 @@
-import { Component, component, html, property } from '../../../src';
+import { Component, component, html, listener, property } from '../../../src';
+import { css } from '../../../src/css';
+import { FocusKeyManager } from '../list-key-manager';
 import './accordion-header';
+import { AccordionHeader } from './accordion-header';
 import './accordion-panel';
 
 @component({
     selector: 'ui-accordion',
+    styles: [css`
+    :host {
+        display: flex;
+        flex-direction: column;
+        background: #fff;
+        background-clip: border-box;
+        box-sizing: border-box;
+        border: var(--border-width, 0.125rem) solid var(--border-color, rgba(0,0,0,.25));
+        border-radius: var(--border-radius, 0.25rem);
+    }
+    `],
     template: () => html`
-    <style>
-        :host {
-            display: flex;
-            flex-direction: column;
-            background: #fff;
-            background-clip: border-box;
-            box-sizing: border-box;
-            border: var(--border-width, 0.125rem) solid var(--border-color, rgba(0,0,0,.25));
-            border-radius: var(--border-radius, 0.25rem);
-        }
-    </style>
     <slot></slot>
     `
 })
 export class Accordion extends Component {
+
+    protected focusManager!: FocusKeyManager<AccordionHeader>;
 
     @property({
         reflectAttribute: false
@@ -31,5 +36,37 @@ export class Accordion extends Component {
         super.connectedCallback();
 
         this.role = 'presentation';
+
+        this.focusManager = new FocusKeyManager(this.querySelectorAll('ui-accordion-header'));
+    }
+
+    @listener({
+        event: 'keydown'
+    })
+    protected handleKeydown (event: KeyboardEvent) {
+
+        this.focusManager.handleKeydown(event);
+    }
+
+    @listener({
+        event: 'mousedown'
+    })
+    protected handleMousedown (event: MouseEvent) {
+
+        if (event.target instanceof AccordionHeader) {
+
+            this.focusManager.setActiveItem(event.target as AccordionHeader);
+        }
+    }
+
+    @listener({
+        event: 'focusin'
+    })
+    protected handleFocus (event: FocusEvent) {
+
+        if (event.target instanceof AccordionHeader) {
+
+            this.focusManager.setActiveItem(event.target as AccordionHeader);
+        }
     }
 }
