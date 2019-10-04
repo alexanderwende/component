c41b8bafc1bfe4516eedaabea6a1e57558ac5dfc
Alexander Wende <alex.wende@gmail.com>
2019-10-04 02:50:29 +0200

    test(select): add select component to test overlay system

---
+++ example/src/select/option.ts
@@ -0,0 +1,58 @@
+import { Component, component, css, property, AttributeConverterARIABoolean, AttributeConverterNumber } from "@partkit/component";
+import { html } from "lit-html";
+
+@component({
+    selector: 'ui-component',
+    styles: [css`
+    :host {}
+    :host([aria-selected=true]) {}
+    `],
+    template: (option: Option) => html`<slot></slot>`
+})
+export class Option extends Component {
+
+    private _disabled = false;
+
+    /**
+     * We provide our own tabindex property, so we can set it to `null`
+     * to remove the tabindex-attribute.
+     */
+    @property({
+        attribute: 'tabindex',
+        converter: AttributeConverterNumber
+    })
+    tabindex!: number | null;
+
+    @property({
+        attribute: 'aria-selected',
+        converter: AttributeConverterARIABoolean
+    })
+    selected = false;
+
+    @property({
+        attribute: 'aria-disabled',
+        converter: AttributeConverterARIABoolean,
+    })
+    get disabled (): boolean {
+
+        return this._disabled;
+    }
+
+    set disabled (value: boolean) {
+
+        this._disabled = value;
+
+        this.tabindex = value ? null : (this.selected ? 0 : -1);
+    }
+
+    @property()
+    role!: string;
+
+    connectedCallback () {
+
+        super.connectedCallback();
+
+        this.role = 'option';
+        this.tabindex = this.disabled ? null : -1;
+    }
+}
