5ddab5b4f20d3f26994c12fc4734a1e54791fab9
Alexander Wende <alex.wende@gmail.com>
2019-11-10 04:39:33 +0100

    refactor(behavior): decouple attached state from element, make element argument optional in attach()

--- example/src/behavior.ts
+++ example/src/behavior.ts
@@ -2,13 +2,15 @@ import { EventBinding, EventManager } from "./event-manager";
 
 export abstract class Behavior {
 
+    protected _attached = false;
+
     protected _element: HTMLElement | undefined;
 
     protected eventManager = new EventManager();
 
     get hasAttached (): boolean {
 
-        return this._element !== undefined;
+        return this._attached;
     }
 
     get element (): HTMLElement | undefined {
@@ -16,9 +18,11 @@ export abstract class Behavior {
         return this._element;
     }
 
-    attach (element: HTMLElement, ...args: any[]) {
+    attach (element?: HTMLElement, ...args: any[]) {
 
         this._element = element;
+
+        this._attached = true;
     }
 
     detach (...args: any[]) {
@@ -26,27 +30,29 @@ export abstract class Behavior {
         this.eventManager.unlistenAll();
 
         this._element = undefined;
+
+        this._attached = false;
     }
 
-    listen (target: EventTarget, type: string, listener: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions) {
+    listen (target: EventTarget, type: string, listener: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions): EventBinding | undefined {
 
-        this.eventManager.listen(target, type, listener, options);
+        return this.eventManager.listen(target, type, listener, options);
     }
 
-    unlisten (target: EventTarget, type: string, listener: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean) {
+    unlisten (target: EventTarget, type: string, listener: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): EventBinding | undefined {
 
-        this.eventManager.unlisten(target, type, listener, options);
+        return this.eventManager.unlisten(target, type, listener, options);
     }
 
     dispatch (event: Event): boolean;
     dispatch<T = any> (type: string, detail?: T, eventInit?: Partial<EventInit>): boolean;
     dispatch<T = any> (eventOrType?: Event | string, detail?: T, eventInit?: Partial<EventInit>): boolean {
 
-        if (this.hasAttached) {
+        if (this.hasAttached && this.element) {
 
             return (eventOrType instanceof Event)
-                ? this.eventManager.dispatch(this.element!, eventOrType)
-                : this.eventManager.dispatch(this.element!, eventOrType!, detail, eventInit);
+                ? this.eventManager.dispatch(this.element, eventOrType)
+                : this.eventManager.dispatch(this.element, eventOrType!, detail, eventInit);
         }
 
         return false;
